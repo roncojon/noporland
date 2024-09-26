c4f9db9488542ea517683f5ed2a86046cd05e6f2
Ronald <rguillen@datamart.cl>
2024-09-26 12:50:06 -0400

    -fix add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
-
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output:'hybrid',
